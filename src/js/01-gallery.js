7e14ba8a3bc17f10409ee41cfc0c804d5c462bc5
Pshenichka1 <pshenichka.olga@gmail.com>
2023-03-17 18:37:31 +0200

    hw-088-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryList = document.querySelector('.gallery');
+const galleryMarkup = handleGalleryMarkup(galleryItems);
+
+function handleGalleryMarkup (items) {
+    return items.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+     <img
+       class="gallery__image"
+       src="${preview}"
+       alt="${description}"
+     />
+   </a>`;
+    }).join("");
+}
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryList.addEventListener('click', handleGalleryClick);
+
+function handleGalleryClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    new SimpleLightbox(".gallery a", {
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
+}
 
 console.log(galleryItems);
