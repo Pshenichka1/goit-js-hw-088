651655c8cd885b740b55b5f7af9b4bd0d9640ff4
Pshenichka1 <pshenichka.olga@gmail.com>
2023-03-18 15:32:56 +0200

    hw-088-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
 
+    player.on('timeupdate', throttle(function(currentTime) {
+        localStorage.setItem("videoplayer-current-time", JSON.stringify(currentTime));
+    }, 1000));
+
+const saveTime = localStorage.getItem("videoplayer-current-time");
+const stopTime = JSON.parse(saveTime);
+player.setCurrentTime(stopTime.seconds || 0).then(function (seconds) { }).
+catch(function(error) {
+    switch (error.name) {
+    case 'RangeError':
+            break;
+            default:
+            break;
+    }
+ }); 
+        
+  
