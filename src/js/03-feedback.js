1beb9c076ec2e84ff0aa8608850ea76bb8cf7c42
Pshenichka1 <pshenichka.olga@gmail.com>
2023-03-21 15:02:32 +0200

    виправлення

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,12 +4,9 @@ const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
 const inputEmail = document.querySelector('input [name="email"]');
 const inputTextarea = document.querySelector('textarea [name="message"]');
-
-textareaMessage();
 let formData = {};
 
 form.addEventListener('input', throttle(handleTextareaInput, 500));
-
 
 function handleTextareaInput(e) {
     formData[e.target.name] = e.target.value;
@@ -24,6 +21,7 @@ function textareaMessage() {
     console.log("email:", inputEmail.value);
     console.log("message:", inputTextarea.value);
 }
+textareaMessage();
 form.addEventListener('submit', handleFormSubmit);
 function handleFormSubmit(e) {
     e.preventDefault();
