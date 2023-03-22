150e862aaee823b2a301e3a7a2bbdd2643ce6687
Pshenichka1 <pshenichka.olga@gmail.com>
2023-03-22 11:21:12 +0200

    ще виправлення

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,8 +2,8 @@ import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
-const inputEmail = document.querySelector('input [name="email"]');
-const inputTextarea = document.querySelector('textarea [name="message"]');
+const inputEmail = document.querySelector('input[name="email"]');
+const inputTextarea = document.querySelector('textarea[name="message"]');
 let feedbackForm = {};
 textareaMessage();
 
@@ -12,8 +12,8 @@ function handleFormSubmit(e) {
     e.preventDefault();
     const formData = new FormData(form);
     formData.forEach((email, message) =>
-        console.log('email: ${email.value}, message: ${message.value}'));
-       
+        console.log(email, message));
+           
     e.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
  };
@@ -25,24 +25,15 @@ function handleTextareaInput(e) {
     localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackForm))
 };
 function textareaMessage() {
-    let savedMessage = localStorage.getItem('feedbackForm');
+    let savedMessage = localStorage.getItem(STORAGE_KEY);
+    
     if (savedMessage) {
         savedMessage = JSON.parse(savedMessage);
+        console.log(inputEmail);
         inputEmail.value = savedMessage.email;
         inputTextarea.value = savedMessage.message;
-        
+  
     } 
 }
 
 
-// form.addEventListener('submit', handleFormSubmit);
-// function handleFormSubmit(e) {
-//     e.preventDefault();
-//     const formData = new FormData(form);
-//     formData.forEach((email, message) => console.log(email, message));
-//     console.log("email:", inputEmail.value);
-//     console.log("message:", inputTextarea.value);
-   
-//     e.currentTarget.reset();
-//     localStorage.removeItem(STORAGE_KEY);
-//  };
