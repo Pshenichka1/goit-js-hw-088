16ae7ba442782e7230314cdf3075272277bbb368
Pshenichka1 <pshenichka.olga@gmail.com>
2023-03-22 10:33:12 +0200

    088

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,29 +4,45 @@ const STORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('.feedback-form');
 const inputEmail = document.querySelector('input [name="email"]');
 const inputTextarea = document.querySelector('textarea [name="message"]');
-let formData = {};
-
-form.addEventListener('input', throttle(handleTextareaInput, 500));
-
-function handleTextareaInput(e) {
-    formData[e.target.name] = e.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
-};
-function textareaMessage() {
-   const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY))
-    if (savedMessage) {
-        inputEmail.value = savedMessage.email;
-        inputTextarea.value = savedMessage.message;
-    } 
-}
+let feedbackForm = {};
 textareaMessage();
 
 form.addEventListener('submit', handleFormSubmit);
 function handleFormSubmit(e) {
     e.preventDefault();
-    console.log("email:", inputEmail.value);
-    console.log("message:", inputTextarea.value);
-   
+    const formData = new FormData(form);
+    formData.forEach((email, message) =>
+        console.log('email: ${email.value}, message: ${message.value}'));
+       
     e.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
  };
+
+form.addEventListener('input', throttle(handleTextareaInput, 500));
+
+function handleTextareaInput(e) {
+    feedbackForm[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackForm))
+};
+function textareaMessage() {
+    let savedMessage = localStorage.getItem('feedbackForm');
+    if (savedMessage) {
+        savedMessage = JSON.parse(savedMessage);
+        inputEmail.value = savedMessage.email;
+        inputTextarea.value = savedMessage.message;
+        
+    } 
+}
+
+
+// form.addEventListener('submit', handleFormSubmit);
+// function handleFormSubmit(e) {
+//     e.preventDefault();
+//     const formData = new FormData(form);
+//     formData.forEach((email, message) => console.log(email, message));
+//     console.log("email:", inputEmail.value);
+//     console.log("message:", inputTextarea.value);
+   
+//     e.currentTarget.reset();
+//     localStorage.removeItem(STORAGE_KEY);
+//  };
